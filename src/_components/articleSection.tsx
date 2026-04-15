b9e0eb2cc6d70071fb3886da25e5b1c808b01411
Taishi Naka <lemonadern@users.noreply.github.com>
2026-04-15 20:27:18 +0900

    fix: restore prose link styling

--- src/_components/articleSection.tsx
+++ src/_components/articleSection.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 
 export const ArticleSection = ({ children }: Props) => (
-  <article class="prose mx-auto max-w-full bg-white px-8 py-8 sm:max-w-[80%] sm:rounded-xl md:max-w-[70%] lg:max-w-[60%] lg:p-12 xl:max-w-[50%] xl:p-16">
+  <article class="prose md-prose mx-auto max-w-full bg-white px-8 py-8 sm:max-w-[80%] sm:rounded-xl md:max-w-[70%] lg:max-w-[60%] lg:p-12 xl:max-w-[50%] xl:p-16">
     {children}
   </article>
 );
